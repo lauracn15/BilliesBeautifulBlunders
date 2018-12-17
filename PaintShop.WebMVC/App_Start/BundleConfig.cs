d2938dd5ee70123c0f1dcfe270b0e3d085fab0e7
lauracn15 <lauracn15@users.noreply.github.com>
2018-12-17 14:36:03 -0500

    best it's ever looked

--- PaintShop.WebMVC/App_Start/BundleConfig.cs
+++ PaintShop.WebMVC/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace PaintShop.WebMVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css", 
+                      "~/Content/Custom.css"));
         }
     }
 }
